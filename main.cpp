a6d1309babe6c5842a490a0c8dc34f6c185d88d9
chanu <chanuka.williams@outlook.com>
2026-04-18 14:11:27 +0100

    Added clang-format for style consistency.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <gtkmm.h>
 
-class MainWindow : public Gtk::Window {
+class MainWindow : public Gtk::Window
+{
 public:
-    MainWindow() {
+    MainWindow()
+    {
         set_title("Hello gtkmm");
         set_default_size(400, 300);
 
@@ -14,7 +16,8 @@ private:
     Gtk::Label m_label;
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     auto app = Gtk::Application::create("org.example.hello");
 
     return app->make_window_and_run<MainWindow>(argc, argv);
